b1491e4a4e795b5cf0044f05e9a491e95bcd9ba6
mogetutu <imogetutu@gmail.com>
2014-06-09 12:59:31 +0300

    removed database require class

--- php-mvc/index.php
+++ php-mvc/index.php
@@ -1,8 +1,6 @@
 <?php
 // Load the config
 require './application/config/config.php';
-// Load the Database Class
-require './application/lib/database.php';
 
 // Load models
 require './application/models/friend.php';
