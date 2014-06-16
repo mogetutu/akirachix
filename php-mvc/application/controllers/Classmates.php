8051cc06918a118d5422b3198e43d8837bd4896c
mogetutu <imogetutu@gmail.com>
2014-06-16 13:55:01 +0300

    added classmates class

---
+++ php-mvc/application/controllers/Classmates.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+*  Classmates
+*/
+class Classmates extends Controller
+{
+
+  function all()
+  {
+    return "All Classmates";
+  }
+
+  public function some()
+  {
+    return "Some Classmates";
+  }
+}
