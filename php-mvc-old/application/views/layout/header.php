d8c0354d59be4f04299aab97c5d45c308bbf35b9
mogetutu <imogetutu@gmail.com>
2014-06-23 11:43:11 +0300

    removed old files

--- php-mvc-old/application/views/layout/header.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-  <title>Sample PHP Assignment</title>
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-</head>
-<body>
-  <div class="container">
