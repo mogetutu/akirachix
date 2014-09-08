536bcf0e93561b9ab72f37dde4fd15be0d3a20f9
mogetutu <imogetutu@gmail.com>
2014-09-08 10:53:24 +0300

    added soft delete to users model

---
+++ sato-hack-laravel/app/database/migrations/2014_09_08_070224_add_soft_delete_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeleteToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table) {
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+		    $table->dropColumn('deleted_at');
+		});
+	}
+
+}
--- sato-hack-laravel/app/models/User.php
+++ sato-hack-laravel/app/models/User.php
@@ -4,10 +4,11 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait, SoftDeletingTrait;
 
 	/**
 	 * The database table used by the model.
@@ -15,6 +16,8 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var string
 	 */
 	protected $table = 'users';
+
+	protected $dates = ['deleted_at'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
